969255842ebfe1ced0d5aa06e5f68fae1be1e437
João Casagrande <pngbighouse@gmail.com>
2023-01-26 01:41:15 -0300

    Correções

--- Exerc2/Program.cs
+++ Exerc2/Program.cs
@@ -1,29 +1,29 @@
-﻿Random randNum = new Random();
-int selectedNumber = randNum.Next(100);
-int[] fibonacci = new int[100];
+﻿int selectedNumber;
+
+// Código para imput de um numero
+/*
+Console.WriteLine("Enter a number: ");
+selectedNumber = Convert.ToInt32(Console.ReadLine());
+*/
+// Código para testes rápidos, gerando numeros aleatórios
+
+Random randNum = new Random();
+selectedNumber = randNum.Next(100);
+
+int n1 = 0, n2 = 1, n3;
 bool validated = false;
 
-fibonacci[0] = 0;
-fibonacci[1] = 1;
+do {
+    n3 = n1 + n2;
+    n1 = n2;
+    n2 = n3;
 
-if (selectedNumber == fibonacci[0] || selectedNumber == fibonacci[1]) {
-    validated = true;
-}
-else {
-    for(int i = 2; i < 100; i++) {
-        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    Console.Write(n3 + " ");
+}while (n3 != selectedNumber && n3 < selectedNumber);
 
-        if (fibonacci[i] == selectedNumber) {
-            validated = true;
-            break;
-        }
-        else if (fibonacci[i] > selectedNumber) {
-            validated = false;
-            break;
-        }
-    }
-}
+if (n3 > selectedNumber) validated = false;
+else if (n3 == selectedNumber) validated = true;
 
-Console.WriteLine("Numero selecionado: " + selectedNumber + ". Esse número" + (validated ? "" : " não")  + " está na sequência de Fibonacci");
+Console.WriteLine("\n\nNumero selecionado: " + selectedNumber + ".\nEsse número" + (validated ? "" : " não")  + " está na sequência de Fibonacci");
 
 
--- Exerc5/Program.cs
+++ Exerc5/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string testString = "Hello! How are you?";
+string reverseString = "";
+
+for (int i = testString.Length - 1; i >= 0; i--) {
+    reverseString += testString[i];
+}
+
+Console.WriteLine(reverseString);
