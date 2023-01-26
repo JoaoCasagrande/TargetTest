d8222412c381498174e08a441224e559ac9463c4
João Casagrande <pngbighouse@gmail.com>
2023-01-25 23:18:03 -0300

    Exerc1

--- Exerc1/Program.cs
+++ Exerc1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int indice = 13, soma = 0, k;
+
+for(k = 0; k < indice; k++) {
+    soma += k;
+}
+
+Console.WriteLine(soma);
+
+//O valor de soma será igual a 78
